39f0ccd52d4921b1f53f52d78869882a3133b64a
ozakisa <ozak.satoshi97@gmail.com>
2013-06-10 02:20:38 +0900

    Update wc.py
    ・ファイルを指定して実行できるように。

--- wc.py
+++ wc.py
@@ -1,8 +1,18 @@
-import sys
+import sys,os
 
-f = sys.stdin
-s = f.read()
-f.close()
+argvs = sys.argv     #コマンドライン引数の取得
+argc = len(argvs) 	#コマンドライン引数の個数の取得
+
+#もし argcが適正でないときには
+if (argc != 3): 
+    print 'Usage: $ python %s target_file making_file' % argvs[0]
+    quit()
+#コメント表示して終了
+
+#適正なときは
+f = open(argvs[1]) 	#対象ファイルから
+s = f.read() 	#全てリストに読み込んで一行ごとに処理
+f.close() 		#ファイル閉じる
 
 words = s.split()
 
